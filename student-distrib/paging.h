a65c225fc29955186857e973c43110cf4cd49c75
Shivam Bharuka <shivam.bharuka@gmail.com>
2016-10-17 01:42:53 -0500

    paging header file

---
+++ student-distrib/paging.h
@@ -0,0 +1,11 @@
+/* paging.h - Defines for paging handler
+ * vim:ts=4 noexpandtab
+ */
+
+#ifndef _PAGING_H
+#define _PAGING_H
+
+#define VIDEO_MEMORY_ADDRESS	0xB8000
+#define KERNEL_MEMORY_ADDRESS	0x400000
+
+#endif /* _PAGING_H */
