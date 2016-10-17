87559589867d72f6272e8c6be90527b0d8108d16
Shivam Bharuka <shivam.bharuka@gmail.com>
2016-10-17 06:15:06 -0500

    paging comments

--- student-distrib/paging.c
+++ student-distrib/paging.c
@@ -1,12 +1,19 @@
 #include "paging.h"
 
+//page directory array
 uint32_t page_directory[1024] __attribute__((aligned(4096)));
+//page table array
 uint32_t page_table[1024] __attribute__((aligned(4096)));
 
-void init_paging() {
-	
-	//uint16_t index_to_directory, index_to_table;
 
+/*
+ * init_paging
+ *   DESCRIPTION: Initialize the paging. 
+ *   INPUTS: none
+ *   OUTPUTS: none
+ *   RETURN VALUE: none
+ */ 
+void init_paging() {
 	/* Set each page directory entry to not present */
 	int i;
 	for(i=0; i < 1024; i++){
@@ -18,11 +25,8 @@ void init_paging() {
 	//page directory for 4MB pages
 	page_directory[1] = KERNEL_MEMORY_ADDRESS | 0x83;
 
-	//page directory for video memory
-	//index_to_table = (VIDEO_MEMORY_ADDRESS & 0x3FF000) >> 12;
-
 	//enable video memory
-	page_table[0xb8] = VIDEO_MEMORY_ADDRESS | 0x3;
+	page_table[INDEX_TO_TABLE] = VIDEO_MEMORY_ADDRESS | 0x3;
 
 	//page directory index points to page table and is set to present
 	page_directory[0] = (uint32_t)page_table | 0x3;
@@ -30,4 +34,3 @@ void init_paging() {
 	loadPageDirectory((uint32_t)page_directory);
 	enablePaging();
 }
-
--- student-distrib/paging.h
+++ student-distrib/paging.h
@@ -7,12 +7,22 @@
 
 #include "types.h"
 
+// video memory address
 #define VIDEO_MEMORY_ADDRESS	0xB8000
+
+// kernel memory address
 #define KERNEL_MEMORY_ADDRESS	0x400000
 
+// page table address for video memory
+#define INDEX_TO_TABLE 			0xB8
+
+/* Initialize the paging */
 extern void init_paging();
 
+/* load page directory */
 extern void loadPageDirectory(unsigned int*);
+
+/* enable paging */
 extern void enablePaging();
 
 #endif /* _PAGING_H */
