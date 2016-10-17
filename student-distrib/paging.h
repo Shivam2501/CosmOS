39478f2bfef370f18ce2452f89791a400d507a71
Shivam Bharuka <shivam.bharuka@gmail.com>
2016-10-17 05:00:35 -0500

    paging header fix

--- student-distrib/paging.h
+++ student-distrib/paging.h
@@ -5,9 +5,14 @@
 #ifndef _PAGING_H
 #define _PAGING_H
 
+#include "types.h"
+
 #define VIDEO_MEMORY_ADDRESS	0xB8000
 #define KERNEL_MEMORY_ADDRESS	0x400000
 
 extern void init_paging();
+
+extern void loadPageDirectory(unsigned int*);
+extern void enablePaging();
 
 #endif /* _PAGING_H */
