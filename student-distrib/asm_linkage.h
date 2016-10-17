0f525e77d30a269fa6d9cf23821c646f3cc07296
Shivam Bharuka <shivam.bharuka@gmail.com>
2016-10-17 05:04:39 -0500

    asm linkage final header

---
+++ student-distrib/asm_linkage.h
@@ -0,0 +1,12 @@
+#ifndef _ASM_LINKAGE_H
+#define _ASM_LINKAGE_H
+
+extern void asm_timer_chip(void);
+extern void asm_keyboard(void);
+extern void asm_rtc(void);
+extern void asm_default(void);
+
+
+
+
+#endif
