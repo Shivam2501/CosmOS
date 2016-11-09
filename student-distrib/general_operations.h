7902d1d829ac68c9102fcadad4c3ed8c6f904957
primeh0 <pmehta11@illinois.edu>
2016-11-09 17:44:51 -0600

    general to syscall

--- student-distrib/general_operations.h
+++ student-distrib/general_operations.h
@@ -32,16 +32,16 @@ typedef struct dir_ops{
 file_array_t FD[8]; 
 
 
-int32_t general_getargs (uint8_t* buf, int32_t nbytes);
-int32_t general_vidmap (uint8_t** screen_start);
-int32_t general_set_handler (int32_t signum, void* handler_address);
-int32_t general_sigreturn (void);
-int32_t general_halt (uint8_t status);
-int32_t general_execute (const uint8_t* command);
-int32_t general_read (int32_t fd, void* buf, int32_t nbytes);
-int32_t general_write (int32_t fd, const void* buf, int32_t nbytes);
-int32_t general_open (const uint8_t* filename);
-int32_t general_close (int32_t fd);
+int32_t syscall_getargs (uint8_t* buf, int32_t nbytes);
+int32_t syscall_vidmap (uint8_t** screen_start);
+int32_t syscall_set_handler (int32_t signum, void* handler_address);
+int32_t syscall_sigreturn (void);
+int32_t syscall_halt (uint8_t status);
+int32_t syscall_execute (const uint8_t* command);
+int32_t syscall_read (int32_t fd, void* buf, int32_t nbytes);
+int32_t syscall_write (int32_t fd, const void* buf, int32_t nbytes);
+int32_t syscall_open (const uint8_t* filename);
+int32_t syscall_close (int32_t fd);
 
 
 
